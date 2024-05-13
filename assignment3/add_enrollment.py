dfbe468610a6d73eb11b9200295060f67a906733
KristofferGus <gusguskrp@student.gu.se>
2024-05-13 16:18:55 +0200

    .

--- assignment3/add_enrollment.py
+++ assignment3/add_enrollment.py
@@ -3,15 +3,15 @@ import csv
 
 if __name__ == "__main__":
     # Open the CSV file in read mode
-    with open('Programme_Courses.csv', newline='') as csvfile:
+    with open('Students.csv', newline='') as csvfile:
         # Create a CSV reader object
         lines = [l for l in csv.reader(csvfile)][1:]
 
         names = [l[1] for l in lines]
-        values = [[l[-1], f"{l[2]}_{l[0]}", l[3]] for l in lines]
-        insert = insert_clazz(clazz="ProgrammeCourse", 
-                     properties=["programmeCourseType"],
-                     objectProps=["programmeCourseAvailableFor", "programCourseInReferanceTo"],
+        values = [[l[-1], l[0], f"{l[-2]}-{int(l[-2])+1}_{l[-3]}"] for l in lines]
+        insert = insert_clazz(clazz="Enrollment",
+                     properties=["enrolmentGraduated"],
+                     objectProps=["enrolment", "enrolment"],
                      values=values, 
                      prefixes=PREFIXES
                      )
--- assignment3/add_programme_instance.py
+++ assignment3/add_programme_instance.py
@@ -1,7 +1,15 @@
 from add_thing import insert_clazz, PREFIXES
 import csv
-from itertools import groupby
 
+def group_by_first_element(tuples):
+    grouped_dict = {}
+    for tup in tuples:
+        key = tup[0]
+        group: set = grouped_dict.get(key, set())
+        group.add(tup[1])
+        grouped_dict[key] = group
+
+    return {k:sorted(list(v)) for k,v in grouped_dict.items()}
 
 if __name__ == "__main__":
     # Open the CSV file in read mode
@@ -11,18 +19,15 @@ if __name__ == "__main__":
 
       
         values = [(l[0], l[2]) for l in lines]
-        # Sort the data based on the first element of each tuple
-        values.sort(key=lambda x: x[0])
+        grouped = group_by_first_element(values)
+        values = sum([[[year,k] for year in v] for k,v in grouped.items()], [])
+        names = [f"{t[0]}_{t[1]}" for t in values]
 
-        # Group the data by the first element of each tuple
-        grouped_data = {key: list(set([g[1] for g in group])) for key, group in groupby(values, key=lambda x: x[0])}
-        print(grouped_data.items())
-"""
-        insert = insert_clazz(clazz="Programme", 
+        insert = insert_clazz(clazz="ProgrammeInstance", 
                      node_names=names,
                      properties=["programmeInstanceYear"],
                      objectProps=["programmeInstanceOf"],
                      values=values, 
                      prefixes=PREFIXES
                      )
-        print(insert)"""
+        print(insert)
