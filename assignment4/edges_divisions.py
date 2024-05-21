ba55725112b6c664ad1f689b5769a4f56eccf375
lukas-gartman <gusgartlu@student.gu.se>
2024-05-21 22:10:52 +0200

    Split divisions into create & edges

--- assignment4/edges_divisions.py
+++ assignment4/edges_divisions.py
@@ -2,7 +2,7 @@ from create_grapth import *
 label = Division.n
 props = [Division.name]
 
-print(create_label(label=label, props=props, uniq_props=[Division.name]))
+print(create_relation(Division.under, [], []))
 
 with open('../Senior_Teachers.csv', newline='') as division_csv:
     lines = division_csv.readlines()[1:]
@@ -11,4 +11,4 @@ with open('../Senior_Teachers.csv', newline='') as division_csv:
     for d in divisions_and_departments:
         dd[d[0]] = d[1]
         
-    print(create_value([Division.n], [Division.name], [[d] for d in dd.keys()]))
+    print(create_edges(Division.under, [], [], [Edge_data(Division.n, [Division.name], [ed[0]], Department.n, [Department.name], [ed[1]], []) for ed in dd.items()]))
