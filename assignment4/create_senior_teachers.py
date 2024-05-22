a4c1be50bf5689c67e937b47eb35b918076d1d0f
KristofferGus <69906487+KristofferGus@users.noreply.github.com>
2024-05-22 16:30:17 +0200

    Revert "added compile"
    This reverts commit db4f3ae1da718d62428f0c0d19a5b1997f8a7a43.

--- assignment4/create_senior_teachers.py
+++ assignment4/create_senior_teachers.py
@@ -4,7 +4,7 @@ props = [Person.id, Person.name]
 
 print(create_label(label=label, props=props, uniq_props=[Person.id]))
 
-with open('Senior_Teachers.csv', newline='') as teacher_csv:
+with open('../Senior_Teachers.csv', newline='') as teacher_csv:
     lines = teacher_csv.readlines()[1:]
     teachers = map(lambda t: [t.split(",")[1], t.split(",")[0], t.split(",")[-1].rstrip('\n')], lines)
     t = dict()
